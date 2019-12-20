087e09ea0062b407b1dde9ed0ef553665facf122
Clara Pasteau <clarapasteau@gmail.com>
2019-12-20 22:02:17 +0100

    add footer, fix js, fix color

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,33 @@
 import "../sass/app.scss";
 
 
+
 $(document).ready(function(){
-    $('.project-imgs').slick();
+    $('.project-imgs').slick({
+        infinite: true,
+        dots: true,
+        centerMode: true,
+        arrows: false,
+    });
+
+    $('.minus').hide();
+    $('.captions').hide();
+    $(".plus").click(function(){
+        $(".captions").slideToggle();
+        $('.plus').hide();
+        $('.minus').show();    
+    });
+    $(".minus").click(function(){
+        $(".captions").slideToggle();
+        $('.minus').hide();
+        $('.plus').show();    
+    });
+
+    var btn = $('.backtotop');
+
+btn.on('click', function(e) {
+    console.log('clicked')
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
   });
